ffbc64e7352832422f73712db434e82914bd4c15
PauliuSliavas <sliavas@me.com>
2023-05-16 07:21:14 +0300

    Updated 2023 05 16

---
+++ tkinter.py
@@ -0,0 +1,9 @@
+from tkinter import *
+root = Tk()
+
+eilute = Label(root, text="Hello world")
+eilute2 = Label(root, text="just a text here")
+
+eilute.grid(row=0, column=0)
+eilute2.grid(row=2, column=2)
+root.mainloop()%                 
